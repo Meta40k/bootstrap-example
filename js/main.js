69c8fc9c136f567e21c27bb903dbd05586d38030
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2021-12-27 15:03:52 +0300

    blog

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(function(){
 
-    baguetteBox.run('.gallery');
+    $(window).on('load', function(){
+        $('.preloader').delay(500).fadeOut('slow', function(){
+            $(this).attr('style', 'display: none !important');
+        });
+    });
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrolToTop').fadeOut();
+        }
+    });
+
+
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({scrollTop : 0}, 800);
+        return false;
+    });
 });
